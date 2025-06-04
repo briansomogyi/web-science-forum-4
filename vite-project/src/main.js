c705464c9070c7cfdb358bf7fcbd6d80cbdd3765
briansomogyi <brian.somogyi@emanuel.ro>
2025-06-04 21:25:51 +0300

    Added vue router

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router';
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
